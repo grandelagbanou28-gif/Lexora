3e58a675518073fd6fbc9ba4bf9f7a52cbc00129
Grandel AGBANOU ( GradenX🥷) <grandelagbanou28@gmail.com>
2026-09-06 03:05:50 +0100

    use git clone for Flutter in Vercel build (tool requires git repo)

--- tool/vercel_build.sh
+++ tool/vercel_build.sh
@@ -6,13 +6,8 @@ cd "$project_dir"
 
 export FLUTTER_HOME="$HOME/flutter"
 if [ ! -x "$FLUTTER_HOME/bin/flutter" ]; then
-  echo "Installing Flutter 3.47.2 (stable)..."
-  mkdir -p "$FLUTTER_HOME"
-  curl -fsSL "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.47.2-stable.tar.xz" \
-    -o /tmp/flutter.tar.xz
-  tar -xJf /tmp/flutter.tar.xz -C /tmp
-  mv /tmp/flutter/* "$FLUTTER_HOME/"
-  rm -rf /tmp/flutter /tmp/flutter.tar.xz
+  echo "Installing Flutter stable..."
+  git clone --depth 1 --branch stable https://github.com/flutter/flutter.git "$FLUTTER_HOME"
 fi
 export PATH="$FLUTTER_HOME/bin:$PATH"
 
